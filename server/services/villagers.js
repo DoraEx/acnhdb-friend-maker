ace03b1d09eb2d63cb888ec19d0bb687ff517769
DoraEx <skinnycarrot@gmail.com>
2020-07-11 00:59:35 -0400

    Small changes to test git config again

--- server/services/villagers.js
+++ server/services/villagers.js
@@ -1,15 +1,15 @@
 const villagerData = require('../data_access/villagers');
 
 const getAll = async () => {
-    return await villagerData.getAll()
+    return await villagerData.getAll();
 }
 
 const getById = async (id) => {
-    return await villagerData.getById(id)
+    return await villagerData.getById(id);
 }
 
 const searchByParameters =  async (params) => {
-    return await villagerData.searchByParameters(params)
+    return await villagerData.searchByParameters(params);
 }
 const getSearchParameters = async () => {
     return await villagerData.getSearchParameters();
