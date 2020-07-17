131b8fd88586a98b503c543b817d34138465f81f
DoraEx <skinnycarrot@gmail.com>
2020-07-16 22:40:30 -0400

    Group similar collections - furniture and apparel

--- db_scripts/run.py
+++ db_scripts/run.py
@@ -2,6 +2,8 @@ import xlrd
 import extract_sheet as es
 from pymongo import MongoClient
 
+apparel_list = ['accessories','bags', 'bottoms', 'clothing_other', 'dress_up',
+                'headwear', 'shoes', 'socks', 'tops']
 client = MongoClient()
 db = client.acnhdb
 
@@ -16,8 +18,12 @@ def run():
         #2. add to collection based on category name
         if len(documents) == 0:
             continue
-        
-        collection = db[documents[0]['category']]
+        if documents[0]['category'] in ['wall_mounted', 'miscellaneous', 'housewares']:
+            collection = db['furniture']
+        elif documents[0]['category'] in apparel_list:
+            collection = db['apparel']
+        else:
+            collection = db[documents[0]['category']]
         collection.insert_many(documents)
     
 if __name__ == '__main__':
